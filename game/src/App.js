216bd0bdb8ff3f79f3ce4799d74286c2e176d7ac
Chris_Wallace <kawallis@icloud.com>
2017-05-23 14:24:34 -0700

    moved things into app and got the scene change working

--- game/src/App.js
+++ game/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Scene from './Scene';
+import CharacterStats from './CharacterStats';
 import './App.css'
 
 class App extends Component {
@@ -11,13 +12,11 @@ class App extends Component {
       currentWeapon: 'stick',
       weapons: [],
       attackDamage: 50,
-      currentScene: null,
-      currentMessage: 'Choose your adventure above'
+      currentScene: "App",
     };
 
     this.initialState = this.state;
     this.changeScene = this.changeScene.bind(this);
-    // this.changeScenario = this.changeScenario.bind(this);
     // this.buttonHandler = this.buttonHandler.bind(this);
     // this.run = this.run.bind(this);
   }
@@ -35,7 +34,7 @@ class App extends Component {
 
     this.setState({
       currentScene: scene,
-      currentMessage: `You look around and see an expansive ${scene}`
+      // currentMessage: `You look around and see an expansive ${scene}`
     });
 
     // if (scene === 'sky') {
@@ -46,67 +45,6 @@ class App extends Component {
     //   this.setState({ currentVehicle: 'elephant'});
     // }
   }
-
-  // changeScenario(e) {
-  //   let { monster} = this.state;
-
-  //   const scene = this.state.currentScene;
-  //   monster.name = `${scene} monster`
-  //   let possibleScenarios = [`LOOKOUT! You have stumbled upon a ${monster.name} !`, `Your ${this.state.vehicle} is attacked by a ${scene} monster!`, `Your ${this.state.vehicle} has been ransacked by a swarm of ${scene} creatures!`]
-
-  //   this.setState({ 
-  //     currentMessage: possibleScenarios[this.getRandomAttack()],
-  //     leftButton: 'Fight',
-  //     rightButton: 'Run',
-  //     scenarios: possibleScenarios, 
-  //     monster
-  //   });
-  // }
-
-  // fightScenario(){
-  //   let { currentHealth, monster } = this.state;
-  //   let userHealth = currentHealth - monster.damage;
-    
-  //   if(currentHealth <= 0){
-  //     this.reset();
-  //     this.setState({
-  //       currentMessage: `You died and washed up on nearby beach pick a new scene to start over`,
-  //     });
-  //   }else{
-  //     this.setState({
-  //       currentHealth: userHealth,
-  //       currentMessage: `${monster.name} ${monster.attacks[this.getRandomAttack()]}`,
-  //       leftButton: 'Hitback',
-  //     })
-  //   }
-
-  // }
-
-  // hitBack() {
-  //   let {monster, attackDamage } = this.state;
-  //   let health = monster.health - attackDamage;
-  //   monster.health = health;
-
-  //   this.setState({
-  //     currentMessage: `You hit the ${monster.name} and did ${attackDamage} amount of damage`,
-  //     leftButton: 'Fight',  
-  //     monster
-  //   })
-  // }
-  
-  // getRandomAttack() {
-  //   return Math.floor(Math.random() * 3);
-  // }
-
-  // buttonHandler () {
-  //   if (this.state.leftButton === 'Continue'){
-  //     this.changeScenario();
-  //   }else if (this.state.leftButton === 'Fight') {
-  //     this.fightScenario();
-  //   }else if (this.state.leftButton === 'Hitback'){
-  //     this.hitBack();
-  //   }
-  // }
 
   // run() {
   //   this.setState({
@@ -123,8 +61,13 @@ class App extends Component {
     let playerObj = { playerName, currentWeapon, weapons, currentHealth, attackDamage};
 
     return (
-        <div className="App">
-          <Scene player={playerObj} currentMessage={currentMessage} scene={currentScene} changeScene={this.changeScene}/>
+        <div className={this.state.currentScene} >
+          <CharacterStats player={playerObj} />
+          <Scene 
+            currentMessage={currentMessage}
+            scene={currentScene} 
+            changeScene={this.changeScene} 
+          />
         </div>
     );
   }
--- game/src/Scene.js
+++ game/src/Scene.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import CharacterStats from './CharacterStats';
 import { Button } from 'react-materialize';
 
 class Scene extends Component {
+
   constructor(props) {
     super(props);
 
@@ -11,20 +11,84 @@ class Scene extends Component {
       rightButton: 'Catch your breath',
       // vehicle: null,
       // scenarios: null,
-      // monster: {
-      //    name: "Monster",
-      //    health: 100,
-      //    damage: 100,
-      //    attacks: ['bashed you with stick', 'ripped off you arm', 'spit on you'],
-      //    isAlive: true
-      // }
+      currentMessage: 'Choose your adventure above',
+      monster: {
+         name: "Monster",
+         health: 100,
+         damage: 100,
+         attacks: ['bashed you with stick', 'ripped off you arm', 'spit on you'],
+         isAlive: true
+      }
+    }
+
+    
+    this.buttonHandler = this.buttonHandler.bind(this);
+  }
+
+    getRandomAttack() {
+      return Math.floor(Math.random() * 3);
+    }
+
+  changeScenario() {
+    let { monster} = this.state;
+    const scene = this.state.currentScene;
+    monster.name = `${scene} monster`
+    let possibleScenarios = [`LOOKOUT! You have stumbled upon a ${monster.name} !`, `Your ${this.state.vehicle} is attacked by a ${scene} monster!`, `Your ${this.state.vehicle} has been ransacked by a swarm of ${scene} creatures!`]
+
+    this.setState({ 
+      currentMessage: possibleScenarios[this.getRandomAttack()],
+      leftButton: 'Fight',
+      rightButton: 'Run',
+      scenarios: possibleScenarios, 
+      monster
+    });
+  }
+
+  fightScenario(){
+    let { currentHealth, monster } = this.state;
+    let userHealth = currentHealth - monster.damage;
+    
+    if(currentHealth <= 0){
+      this.reset();
+      this.setState({
+        currentMessage: `You died and washed up on nearby beach pick a new scene to start over`,
+      });
+    }else{
+      this.setState({
+        currentHealth: userHealth,
+        currentMessage: `${monster.name} ${monster.attacks[this.getRandomAttack()]}`,
+        leftButton: 'Hitback',
+      })
+    }
+
+  }
+
+  hitBack() {
+    let {monster, attackDamage } = this.state;
+    let health = monster.health - attackDamage;
+    monster.health = health;
+
+    this.setState({
+      currentMessage: `You hit the ${monster.name} and did ${attackDamage} amount of damage`,
+      leftButton: 'Fight',  
+      monster
+    })
+  }
+
+  buttonHandler () {
+    let {leftButton} = this.state;
+    if (leftButton === 'Continue'){
+      this.changeScenario();
+    }else if (leftButton === 'Fight') {
+      this.fightScenario();
+    }else if (leftButton === 'Hitback'){
+      this.hitBack();
     }
   }
 
+
   render() {
     return (
-      <div className={`Scene ${this.props.scene}`}>
-        <CharacterStats player={this.props.player} />
         <div id="gameBoard">
           <div>
             <Button className="scenes" waves='light' onClick={this.props.changeScene}>Sky</Button>
@@ -33,7 +97,7 @@ class Scene extends Component {
           </div>
           <div>
             <h1>
-              {this.props.currentMessage}
+              {this.state.currentMessage}
             </h1>
           </div>
           <div id="actionButtons">
@@ -41,7 +105,6 @@ class Scene extends Component {
             <Button className="Button" waves='light' onClick={this.run}>{this.state.rightButton}</Button>
           </div>
         </div>
-      </div>
     );
   }
 }
